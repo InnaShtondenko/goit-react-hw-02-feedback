83a69f5d06a00de2a839264fe7e914cd9dee6d43
InnaShtondenko <inna.mycase@gmail.com>
2022-11-01 14:38:56 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,68 @@
-import  userData  from 'data/user.json';
-import statisticsData from 'data/data.json';
-import friendsData from 'data/friends.json';
-import transactionData from 'data/transactions.json';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './utils/Theme.styled';
-import { Profile } from 'components/Profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './common/Section/Section.styled';
+import { Statistics } from './FeedbackStatistics/FeedbackStatistics';
 
-export const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Profile
-          username={userData.username}
-          tag={userData.tag}
-          location={userData.location}
-          avatar={userData.avatar}
-          stats={userData.stats}
-      />
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-      <Statistics title="Upload stats" stats={statisticsData} />
-      <Statistics stats={statisticsData} />
-      <FriendList friends={friendsData} />
-      <TransactionHistory items={transactionData} />
-    </ThemeProvider>
-  );
-};
+  total = 0;
+  positivePercentage = null;
+
+  calcPositivePercentage() {
+    const { good } = this.state;
+    const { total } = this;
+
+    if (!total) return;
+
+    this.positivePercentage = parseInt((good / total) * 100);
+  }
+
+  calcTotalNumberFeedbacks() {
+    const { good, neutral, bad } = this.state;
+
+    this.total = good + neutral + bad;
+  }
+
+  onFeedbackOptClicked = type => {
+    this.setState(prevState => {
+      return { [type]: prevState[type] + 1 };
+    });
+  };
+
+  onRender() {
+    this.calcTotalNumberFeedbacks();
+    this.calcPositivePercentage();
+  }
+
+  render() {
+    this.onRender();
+    const { good, neutral, bad } = this.state;
+    const { total, positivePercentage } = this;
+
+    return (
+      <>
+        <Section title="Please leave a feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onFeedbackOptClicked}
+          />
+        </Section>
+
+        <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+        </Section>
+      </>
+    );
+  }
+}
